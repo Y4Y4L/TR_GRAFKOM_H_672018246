83de57cd9ecf5494599eb6290fc5c3e86c710696
Y4Y4L <86673999+Y4Y4L@users.noreply.github.com>
2021-06-29 22:21:25 +0700

    Update Ontario College of Art and Design.c

--- Ontario College of Art and Design.c
+++ Ontario College of Art and Design.c
@@ -929,7 +929,7 @@ int main (int argc, char **argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(800, 600);
     glutInitWindowPosition(250, 80);
-    glutCreateWindow("3D");
+    glutCreateWindow("Ontario College of Art and Design");
     init();
     glutDisplayFunc(tampil);
     glutKeyboardFunc(keyboard);
